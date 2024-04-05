60ea4dff0ee460d48384d115e3957f0ae5ccac93
juanchoalcoba <juanchoalcoba@gmail.com>
2024-04-05 14:21:26 -0300

    update

--- app.js
+++ app.js
@@ -1,62 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  // Obtener elementos relevantes
 const modal = document.getElementById("modal");
 const btnVerTrailer = document.getElementById("ver-trailer");
 const spanClose = document.getElementsByClassName("close")[0];
 const iframe = document.getElementById("video");
 
-// Mostrar el modal cuando se hace clic en el botón "Ver Trailer"
+
 btnVerTrailer.onclick = function() {
   modal.style.display = "block";
 }
 
-// Ocultar el modal cuando se hace clic en el botón de cierre (x)
-// Ocultar el modal y detener la reproducción del video cuando se hace clic en el botón de cierre (x)
+
 spanClose.onclick = function() {
   modal.style.display = "none";
-  // Detener la reproducción del video
+ 
   const iframeSrc = iframe.src;
-  iframe.src = iframeSrc; // Esto detiene la reproducción del video y lo reinicia
+  iframe.src = iframeSrc; 
 }
 
-// Ocultar el modal y detener la reproducción del video cuando se hace clic fuera del modal
+
 window.onclick = function(event) {
   if (event.target === modal) {
     modal.style.display = "none";
-    // Detener la reproducción del video
+   
     const iframeSrc = iframe.src;
-    iframe.src = iframeSrc; // Esto detiene la reproducción del video y lo reinicia
+    iframe.src = iframeSrc; 
   }
 }
 
 
 var menuItems = document.querySelectorAll('.menu li a');
 
-// Iterar sobre cada enlace y agregar un evento de clic
+
 menuItems.forEach(function(item) {
   item.addEventListener('click', function(event) {
-    // Eliminar la clase 'active' de todos los elementos de enlace
+  
     menuItems.forEach(function(item) {
       item.classList.remove('active');
     });
-    // Agregar la clase 'active' al enlace clickeado
+   
     event.target.classList.add('active');
   });
 });
 
 
-  // Variables para almacenar el índice actual del contenido
+
   let currentIndex = 0;
 
-  // Obtener referencias a los elementos relevantes
+ 
   const poster = document.querySelector(".poster");
   const title = document.querySelector(".title");
   const description = document.querySelector(".description");
   const arrowLeft = document.querySelector(".arrow-left");
   const arrowRight = document.querySelector(".arrow-right");
 
-  // Arreglo de información de las películas
+ 
   const movies = [
     {
       posterUrl:
@@ -87,7 +85,7 @@ menuItems.forEach(function(item) {
     },
   ];
 
-  // Función para actualizar el contenido con el índice dado
+
   function updateContent(index) {
     const movie = movies[index];
     poster.src = movie.posterUrl;
@@ -95,29 +93,29 @@ menuItems.forEach(function(item) {
     description.textContent = movie.description;
   }
 
-  // Función para manejar el clic en la flecha derecha
+
   arrowRight.addEventListener("click", function () {
     if (currentIndex < movies.length - 1) {
       currentIndex++;
       updateContent(currentIndex);
-      // Mostrar la flecha izquierda si no estamos en el primer elemento
+ 
       arrowLeft.style.display = "inline";
     }
-    // Ocultar la flecha derecha si estamos en el último elemento
+  
     if (currentIndex === movies.length - 1) {
       arrowRight.style.display = "none";
     }
   });
 
-  // Función para manejar el clic en la flecha izquierda
+
   arrowLeft.addEventListener("click", function () {
     if (currentIndex > 0) {
       currentIndex--;
       updateContent(currentIndex);
-      // Mostrar la flecha derecha si no estamos en el último elemento
+  
       arrowRight.style.display = "inline";
     }
-    // Ocultar la flecha izquierda si estamos en el primer elemento
+
     if (currentIndex === 0) {
       arrowLeft.style.display = "none";
     }
@@ -130,7 +128,6 @@ menuItems.forEach(function(item) {
     var movie = document.getElementById("movie").value;
     var name = document.getElementById("name").value;
 
-    // Redirigir a la página de resultado con los parámetros en la URL
     window.location.href = "resultado.html?movie=" + encodeURIComponent(movie) + "&name=" + encodeURIComponent(name);
   });
 
